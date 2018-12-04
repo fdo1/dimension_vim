c1e8ff7b2e48c8bb97689d0a9ef691468d134611
Fernando <fernando.madser@gmail.com>
2018-12-03 21:41:13 -0600

    Correct text parsing and displaying

--- scripts/gamefield/code-text.js
+++ scripts/gamefield/code-text.js
@@ -2,30 +2,40 @@ class CodeText {
   constructor(){
     this.levelCodeText =
       "import '/modules/level-1'^"
-     + "var lib = require('/lib')";
+      + "var lib = require('/lib')^"
+      + "var index = lib.getLevelIndex();^"
+      + "function loadLevel(params) {^"
+      + "var something = new Something()^"
+      + "something.displaySomething();^"
+      + "var anotherThing = new Another() }^"
+      + "var something = new Something()^"
+      + "something = anotherThing.reset()^"
+      + "console.log('This is the end of line')";
+
     this.constants = new Constants();
   }
 
   fillText (context) {
+    var _this = this;
     var positionYIndex = 0;
+    var positionXIndex = 0;
 
-    this.levelCodeText.split('').forEach(c => console.log(c));
-
-    for(var i = 0; i < this.levelCodeText.length; ++i ) {
-      var character = this.levelCodeText.substr(i, 1);
-      if(character == '^') {
-        positionYIndex++;
+    this.levelCodeText.split('').forEach(function(c, index){
+      var character = _this.levelCodeText.substr(index, 1);
+      ++positionXIndex;
+      if(character=='^') {
         character = '';
+        positionYIndex++;
+        positionXIndex = 0;
       }
-      var positionY = this.constants.tile.height/2 + this.constants.tile.height * positionYIndex;
-      var positionXIndex = i + 1;
-      var positionX = this.constants.tile.width/2 + this.constants.tile.width * positionXIndex;
+      var positionY = _this.constants.tile.height/2 + _this.constants.tile.height * positionYIndex;
+      var positionX = _this.constants.tile.width/2 + _this.constants.tile.width * positionXIndex;
 
-      context.font = `${this.constants.fontSize}pt Calibri`;
+      context.font = `${_this.constants.fontSize}pt Arial`;
       context.fillStyle = "white";
       context.textAlign = "center";
       context.textBaseline = 'middle';
       context.fillText(character, positionX, positionY); 
-    }
+    });
   }
 }
